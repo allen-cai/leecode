2af65bab75b9edd7626c9f36f7657ce4a42e1c93
cailun <cl30240@163.com>
2020-12-30 15:41:54 +0800

    第四题 两数之和

---
+++ src/java/issuelist/two_sum_1/Solution.java
@@ -0,0 +1,40 @@
+package issuelist.two_sum_1;
+
+/**
+ * @Description
+ * @Author allen
+ * @Date 2020/12/30 15:27
+ * @ClassName Solution
+ **/
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+
+    /**
+     * 标准答案
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
